bb4848027fb6e35031644e820c666a20842a813a
Jesse Rivera <jrivera@sccadv.com>
2017-03-20 15:56:55 -0500

    Read db credentials from environment variables

--- database.go
+++ database.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"os"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func Database() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:root@/release_catalog?charset=utf8&parseTime=True&loc=Local")
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+
+	db, err := gorm.Open("mysql", username+":"+password+"@/release_catalog?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
